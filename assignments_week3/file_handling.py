2fddfef3503eea02e8c854f0f0e92bac6afab37e
1233iughe <santi28.8.99@gmail.com>
2023-04-26 18:58:29 -0600

    forgotten files

--- assignments_week3/file_handling.py
+++ assignments_week3/file_handling.py
@@ -10,7 +10,52 @@ import os
 def info(path):
     if os.path.exists(path):
         file = open(path)
-        sentences = file.read()
+        allfile = file.read()
         file.close()
-        print(sentences)
+        lines = str(allfile.count('\n'))
+        words = str(len(allfile.split()))
+        char = str(len(allfile))
+        print("N° of lines: " + lines, "N° of words: " + words, "N° of characters: " + char)
 
+info('./file_handling.txt')
+
+import csv
+def csv_dic(path):
+    if os.path.exists(path):
+        file = open(path)
+        rows = csv.reader(file)
+        dicto = {f'row{i}':row for i, row in enumerate(rows)}
+        file.close()
+        return dicto
+print(csv_dic('./example.csv'))
+
+def binary_hexa(path):
+    if os.path.exists(path):
+        file = open(path, 'rb')
+        data = file.read().hex()
+        file.close()
+        file = open("hexa.txt", 'w')
+        file.write(data)
+        file.close()        
+binary_hexa('./file.bin')
+
+def adder(path):
+    if os.path.exists(path):
+        file = open(path)
+        data = file.read().split()
+        file.close()
+        return sum([int(num) for num in data])
+print(adder('./numbers.txt'))
+
+def blanks(path):
+    if os.path.exists(path):
+        file = open(path,'r+')
+        data = file.read().split('\n')
+        no_blank = [x for x in data if x]
+        new = '\n'.join(no_blank)
+        file.seek(0)
+        file.write(new)
+        file.truncate()
+        file.close()
+
+blanks('./blanks.txt')
